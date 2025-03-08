bcd9c843cb81005d182ac7341423915a62af792a
SAGARA,Lee Alfie A. <leealfiesagara03@gmail.com>
2025-03-08 11:55:41 +0800

    Create LQ2_1.js

---
+++ LQ2_1.js
@@ -0,0 +1,15 @@
+//SAGARA,lee Alfie A.
+//CS-1B 
+
+
+
+let password = "pogikopri";
+let user_password = prompt("anopassmopogi ");
+let user_username = prompt("anousernamemo? ");
+
+if (user_password == password) {
+alert ('welcome ${user_name}');
+}
+else {
+alert("password mo palitan mo wrong ka kupal")
+}
